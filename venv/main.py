0458af29224211846f22a45b42e0596c5fdb9eda
voronik1801 <divi1801@icloud.com>
2021-04-16 19:40:46 +0700

    add bayes classificator

--- venv/main.py
+++ venv/main.py
@@ -1,7 +1,10 @@
-import pandas as pd
-import numpy as np
 import csv
+
+from sklearn.metrics import accuracy_score
+
 from CFS.CFS import cfs
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
 
 with open('names.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
@@ -13,6 +16,7 @@ p = len(dataForAnalys[0])
 data = np.array(dataForAnalys)
 
 Y = data[:, 10]
+Y = np.float64(Y)
 
 predictX = np.zeros((n, p - 1))
 for k in range(p - 1):
@@ -22,4 +26,11 @@ S = cfs(predictX, Y)
 newPredictX = np.zeros((n, len(S)))
 for k in range(len(S)):
     newPredictX[:, k] = data[:, S[k]]
-print(newPredictX)
+
+
+model = GaussianNB()
+model.fit(newPredictX, Y)
+
+predicted = model.predict(newPredictX)
+
+print(accuracy_score(predicted, Y))
